6da1c9da8093abdfb4a9ac0dffcdcadbfef1b606
code-albertt-hdz <albertthdz.ah@gmail.com>
2023-02-13 21:01:57 -0600

    Proyecto 2 de python

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Django App
-## The second title
+# DJANGO CRUD WITH AUTENTICATION
+## Task Application 
 
-###### The third title 
+###### For install, clone repository and install requirements.txt
+
+pip install virtualenv (inside cloned directory) **if your first time work to virtual environment**
+
+py -m venv venv (inside cloned directory) **if your has been work to virtual environment**
+
+pip install -r requirements.txt
+## Ejecute virtual environment 
+
+.\venv\Scripts\activate 
+
+To run application
+
+## py manage.py runserver
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG=config('DEBUG',cast=bool)
 
 #HIDE DJANGO SECRET KEY
 
